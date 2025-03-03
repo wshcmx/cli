49c216d0072ca96bab0f41a0cea8f7b2ae50a830
umbrik <693254+umbrik@users.noreply.github.com>
2025-03-03 16:54:31 +0100

    Add multiple test projects and call directrly project workspace from the root

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,9 +3,9 @@ import { styleText } from 'node:util';
 import { commands } from './index.js';
 
 export function help() {
-  console.log(styleText('yellow', '\n@wshcmx/cli plugin - cli to work with WebSoft HCM easily!'));
+  console.log(styleText('yellow', '\n@wshcmx/cli plugin - CLI для работы с WebSoft HCM'));
   console.log(`
-Available cli commands:
+Команды для вызова:
 \t${Array.from(commands).map(x => `"${styleText('greenBright', x[0])}" - ${x[1].description}`).join('\n\t')}
 `);
 }
