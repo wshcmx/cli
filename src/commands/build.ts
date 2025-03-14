f95a543596c4363ba5a9b1a8433317a9feb2a85d
umbrik <693254+umbrik@users.noreply.github.com>
2025-03-14 20:40:30 +0100

    Change building output

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -4,12 +4,12 @@ import { getTSConfig } from '../core/config.js';
 import { logger } from '../core/logger.js';
 
 export function build(cwd: string) {
-  logger.success(`🔨 ${new Date().toLocaleTimeString()} Building started`);
+  logger.success(`🔨 ${new Date().toLocaleTimeString()} Project building started`);
   const configuration = getTSConfig(cwd, args.getArg('project'));
 
   buildTypescriptFiles(configuration);
   buildNonTypescriptFiles(configuration);
 
-  logger.success(`✅ ${new Date().toLocaleTimeString()} Build finished`);
+  logger.success(`✅ ${new Date().toLocaleTimeString()} Project building finished`);
   process.exit(0);
 }
