01d033c87e3dbdce86398729597fb5468dee135e
umbrik <693254+umbrik@users.noreply.github.com>
2025-03-14 09:09:55 +0100

    Rework template string interpolation leading and trailing empty strings

--- src/transformers/template_strings.ts
+++ src/transformers/template_strings.ts
@@ -4,20 +4,31 @@ export function convertTemplateStrings(): ts.TransformerFactory<ts.SourceFile> {
   return (context) => (sourceFile: ts.SourceFile) => {
     function visit(node: ts.Node): ts.Node {
       if (ts.isTemplateExpression(node)) {
-        let result: ts.Expression = ts.factory.createStringLiteral(node.head.text);
+        let expression: ts.Expression | undefined;
 
-        node.templateSpans.forEach(span => {
-          const expression = ts.visitNode(span.expression, visit) as ts.Expression;
-          const literal = ts.factory.createStringLiteral(span.literal.text);
+        if (node.head.text.length > 0) {
+          expression = ts.factory.createStringLiteral(node.head.text);
+        }
 
-          result = ts.factory.createBinaryExpression(
-            ts.factory.createBinaryExpression(result, ts.SyntaxKind.PlusToken, expression),
-            ts.SyntaxKind.PlusToken,
-            literal
-          );
-        });
+        for (const span of node.templateSpans) {
+          const visitedExpression = ts.visitNode(span.expression, visit) as ts.Expression;
 
-        return result;
+          if (!expression) {
+            expression = visitedExpression;
+          } else {
+            expression = ts.factory.createBinaryExpression(expression, ts.SyntaxKind.PlusToken, visitedExpression);
+          }
+
+          if (span.literal.text.length > 0) {
+            expression = ts.factory.createBinaryExpression(
+              expression,
+              ts.SyntaxKind.PlusToken,
+              ts.factory.createStringLiteral(span.literal.text)
+            );
+          }
+        }
+
+        return expression ?? ts.factory.createStringLiteral('');
       }
 
       return ts.visitEachChild(node, visit, context);
