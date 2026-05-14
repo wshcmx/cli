5020614943e46bba63b920f79576d2f266dfcea8
umbrik <693254+umbrik@users.noreply.github.com>
2026-05-14 10:17:54 +0200

    Remove version check script and related configurations

--- .github/scripts/check-version.sh
+++
@@ -1,6 +0,0 @@
-localVersion=$(cat package.json | jq -r '.version')
-masterVersion=$(git show origin/main:package.json | jq -r '.version')
-if [ "${localVersion}" = "`echo -e "${localVersion}\n${masterVersion}" | sort -V | head -n1`" ]; then
-    echo '::error::Package version should be upgraded'
-    exit 1
-fi
