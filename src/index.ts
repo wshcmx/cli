3027699f99bfccabe6c8011777905cede523497b
umbrik <693254+umbrik@users.noreply.github.com>
2026-08-02 19:48:06 +0200

    Refactor CLI commands (#36)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,41 @@
 #!/usr/bin/env node
 
-import * as args from './core/args.js';
-import { commands } from './commands/index.js';
-import { help } from './commands/help.js';
+import { parseArgs } from "node:util";
 
-const cwd = process.cwd();
+import { buildTypescriptFiles, watchTypescriptFiles } from './core/build.js';
+import { getTSConfig } from './core/config.js';
 
-const command = commands.get(args.command);
+export const { values: args, positionals: [command] } = parseArgs({
+  options: {
+    help: { type: 'boolean', short: 'h', default: false },
+    version: { type: 'boolean', short: 'v', default: false },
+    project: { type: 'string', short: 'p', default: 'tsconfig.json' },
+  },
+  allowPositionals: true
+});
 
-if (command) {
-  command.callback(cwd);
+const CWD = process.cwd();
+
+if (command === 'build') {
+  console.log(`🔨 ${new Date().toLocaleTimeString()} Project building started`);
+  const configuration = getTSConfig(CWD, args['project']);
+
+  const result = buildTypescriptFiles(configuration);
+
+  if (result?.emitSkipped) {
+    console.error(`❌ ${new Date().toLocaleTimeString()} Project building failed`);
+    process.exit(1);
+  }
+
+  console.log(`✅ ${new Date().toLocaleTimeString()} Project building finished`);
+} else if (command === 'watch') {
+  const configuration = getTSConfig(CWD, args['project']);
+  watchTypescriptFiles(configuration);
 } else {
-  help();
+    console.warn('\n@wshcmx/cli plugin - CLI для работы с WebSoft HCM');
+    console.log(`
+  Команды для вызова:
+  build - сборка проекта
+
+  `);
 }
